a4ad0ec0873b080ea68fe58ae519f7962ca3e53a
John Pena <jpena91@gmail.com>
2019-02-06 13:08:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@ num = 0
 def countdown(num)
   while num > 0 
   num -= 1
-  puts "#{num+1} SECONDS(S)!"
+  p "#{num+1} SECONDS(S)!"
 end
 if num == 0 
-  puts "HAPPY NEW YEAR!"
+  p "HAPPY NEW YEAR!"
 end
 end
